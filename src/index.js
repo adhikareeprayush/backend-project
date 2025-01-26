cc1eb6901456fd3b0d2740aae61bb8da8edf075b
adhikareeprayush <adhikareeprayush@gmail.com>
2025-01-26 15:13:13 +0545

    connect to mongo

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
+
+dotenv.config();
+connectDB();
+
+
+/*
+import express from 'express';
+
+const app = express();
+
+;(async () => {
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error", (error) => {
+           console.error(`Error: ${error}`);
+           throw error;
+       });
+
+       app.listen(3000, () => {
+           console.log(`Server is running on port ${process.env.PORT}`);
+       });
+
+    } catch(error) {
+        console.error(error);
+        throw error;
+    }
+})
+
+connectDB();
+
+*/
